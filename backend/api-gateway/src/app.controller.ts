53728bddeca4a2e2788b491f760fa6e1f90984dd
JcJet <jcjet88@gmail.com>
2023-07-14 17:24:08 +0300

    minor refactor

--- backend/api-gateway/src/app.controller.ts
+++ backend/api-gateway/src/app.controller.ts
@@ -1,43 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import {
-  ApiExcludeEndpoint,
-  ApiOkResponse,
-  ApiOperation,
-  ApiTags,
-} from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
-import { GenresService } from './genres/genres.service';
 
 @Controller()
 @ApiTags('Main application (SERVICE USE ONLY)')
 export class AppController {
-  constructor(
-    private appService: AppService,
-    private genresService: GenresService,
-  ) {}
-
-  @Get('/loadDatabases1337')
-  @ApiExcludeEndpoint()
-  @ApiOperation({
-    summary: 'Load countries, genres & movies data into empty databases.',
-    description:
-      'Should be executed every time, when databases are empty - first start, cleaning DBs (deleting Docker containers) etc.',
-  })
-  @ApiOkResponse({ description: 'All done!' })
-  loadDatabase(): Promise<object> {
-    return this.appService.loadDatabases();
-  }
-
-  @Get('/navigation')
-  @ApiOperation({
-    summary: 'Returns header links.',
-    description:
-      'Returns dynamic header links object, that depends on existing genres.',
-  })
-  @ApiOkResponse({ description: 'All fine!' })
-  getHeaderStaticLinks(): Promise<Observable<object>> {
-    return this.genresService.getHeaderStaticLinks();
-  }
+  constructor(private appService: AppService) {}
 }
